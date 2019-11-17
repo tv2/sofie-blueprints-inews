5807a4af3b42ec5f6fa1384f440177f400218daf
Jesper Stærkær <jesper@starkar.no>
2019-11-17 16:47:47 +0100

    have a look at this tom

--- src/tv2_afvd_studio/config-manifests.ts
+++ src/tv2_afvd_studio/config-manifests.ts
@@ -39,7 +39,7 @@ export const studioConfigManifest: ConfigManifestEntry[] = [
 		description: 'Skype number to ATEM input (eg 1:6,2:7)',
 		type: ConfigManifestEntryType.STRING,
 		required: false,
-		defaultVal: '1:6,2:7,3:8,4:9,5:10,6:11,7:12'
+		defaultVal: '1:6,2:7,3:8,4:9,5:10,6:11,7:12' // @todo: is this correct tom??
 	},
 	{
 		id: 'HyperdeckCount',
