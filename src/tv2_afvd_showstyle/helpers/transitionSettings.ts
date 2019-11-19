df8c4e523453c9d308ab53666de48e46d76e12b5
Thomas Lee <thomasslee97@gmail.com>
2019-11-19 17:18:42 +0000

    fix: Wipe duration

--- src/tv2_afvd_showstyle/helpers/transitionSettings.ts
+++ src/tv2_afvd_showstyle/helpers/transitionSettings.ts
@@ -1,19 +1,18 @@
 import { AtemTransitionSettings } from 'timeline-state-resolver-types'
 import { PartDefinition } from '../inewsConversion/converters/ParseBody'
-import { TimeFromFrames } from '../parts/time/frameTime'
 
 export function TransitionSettings(part: PartDefinition): AtemTransitionSettings {
 	if (part.transition && part.transition.duration) {
 		if (part.transition.style === 'WIPE') {
 			return {
 				wipe: {
-					rate: TimeFromFrames(part.transition.duration)
+					rate: part.transition.duration
 				}
 			}
 		}
 		return {
 			mix: {
-				rate: TimeFromFrames(part.transition.duration)
+				rate: part.transition.duration
 			}
 		}
 	}
