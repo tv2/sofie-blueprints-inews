36bcbf8abc09def9e80eecfc8e0fb237e4b5667e
Jesper Stærkær <jesper@starkar.no>
2019-11-19 18:09:56 +0100

    fix: cleans up sisyfos server channels

--- src/tv2_afvd_studio/layers.ts
+++ src/tv2_afvd_studio/layers.ts
@@ -76,6 +76,8 @@ export enum SisyfosLLAyer {
 	SisyfosSourceLive_8 = 'sisyfos_source_live_8',
 	SisyfosSourceLive_9 = 'sisyfos_source_live_9',
 	SisyfosSourceLive_10 = 'sisyfos_source_live_10',
+	SisyfosSourceServerA = 'sisyfos_source_server_a',
+	SisyfosSourceServerB = 'sisyfos_source_server_b',
 	SisyfosSourceServerC = 'sisyfos_source_server_c',
 	SisyfosSourceEVS_1 = 'sisyfos_source_evs_1',
 	SisyfosSourceEVS_2 = 'sisyfos_source_evs_2'
