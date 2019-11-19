bef78c59a65d2d3b5a14ec55dfdbec6f1435cb6f
Jesper Stærkær <jesper@starkar.no>
2019-11-19 15:01:00 +0100

    feat: Sisyfos baselines This PR sketches out an idea of configuring the default Sisyfos setup in an typed object, and sets both studio- and rundown baseline from this.
    Server-channels need to be calculated from a config specifying how many channels ar eused for servers.
    
    Experimental, un-tested. Sent to @thomaslee97 for a sanity check and being working into a mergeable state.

---
+++ src/tv2_afvd_studio/sisyfosChannels.ts
@@ -0,0 +1,155 @@
+import { SisyfosLLAyer } from './layers'
+
+export interface SisyfosChannel {
+	isPgm: 0 | 1 | 2
+	label: string
+	visibleInStudioA?: boolean
+	visibleInStudioB?: boolean
+}
+
+export const sisyfosChannels: { [key in SisyfosLLAyer]?: SisyfosChannel } = {
+	[SisyfosLLAyer.SisyfosSourceJingle]: {
+		isPgm: 0,
+		label: 'JINGLE',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceAudio]: {
+		isPgm: 0,
+		label: 'AUDIO',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceTLF]: {
+		isPgm: 0,
+		label: 'TLF',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceHost_1_ST_A]: {
+		isPgm: 0,
+		label: 'VÆRT 1',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceHost_2_ST_A]: {
+		isPgm: 0,
+		label: 'VÆRT 2',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_1_ST_A]: {
+		isPgm: 0,
+		label: 'GÆST 1',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_2_ST_A]: {
+		isPgm: 0,
+		label: 'GÆST 2',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_3_ST_A]: {
+		isPgm: 0,
+		label: 'GÆST 3',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_4_ST_A]: {
+		isPgm: 0,
+		label: 'GÆST 4',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceHost_1_ST_B]: {
+		isPgm: 0,
+		label: 'VÆRT 1',
+		visibleInStudioB: true
+	},
+	[SisyfosLLAyer.SisyfosSourceHost_2_ST_B]: {
+		isPgm: 0,
+		label: 'VÆRT 2',
+		visibleInStudioB: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_1_ST_B]: {
+		isPgm: 0,
+		label: 'GÆST 1',
+		visibleInStudioB: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_2_ST_B]: {
+		isPgm: 0,
+		label: 'GÆST 2',
+		visibleInStudioB: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_3_ST_B]: {
+		isPgm: 0,
+		label: 'GÆST 3',
+		visibleInStudioB: true
+	},
+	[SisyfosLLAyer.SisyfosSourceGuest_4_ST_B]: {
+		isPgm: 0,
+		label: 'GÆST 4',
+		visibleInStudioB: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_1]: {
+		isPgm: 0,
+		label: 'LIVE 1',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_2]: {
+		isPgm: 0,
+		label: 'LIVE 2',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_3]: {
+		isPgm: 0,
+		label: 'LIVE 3',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_4]: {
+		isPgm: 0,
+		label: 'LIVE 4',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_5]: {
+		isPgm: 0,
+		label: 'LIVE 5',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_6]: {
+		isPgm: 0,
+		label: 'LIVE 6',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_7]: {
+		isPgm: 0,
+		label: 'LIVE 7',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_8]: {
+		isPgm: 0,
+		label: 'LIVE 8',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_9]: {
+		isPgm: 0,
+		label: 'LIVE 9',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceLive_10]: {
+		isPgm: 0,
+		label: 'LIVE 10',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceEVS_1]: {
+		isPgm: 0,
+		label: 'EVS 1',
+		visibleInStudioA: true
+	},
+	[SisyfosLLAyer.SisyfosSourceEVS_2]: {
+		isPgm: 0,
+		label: 'EVS 2',
+		visibleInStudioA: true
+	}
+}
+
+export function sisyfosServerChannel(channelIndex: number | string): SisyfosChannel {
+	return {
+		isPgm: 0,
+		label: `SERVER ${channelIndex}`,
+		visibleInStudioA: true,
+		visibleInStudioB: true
+	}
+}
