a985653fcb50604f8c2bafdb802e7a1f2d8f8735
Jesper Stærkær <jesper@starkar.no>
2019-11-25 14:31:14 +0100

    fix: unbreak [my heart]

--- src/tv2_afvd_showstyle/parts/live.ts
+++ src/tv2_afvd_showstyle/parts/live.ts
@@ -35,7 +35,7 @@ export function CreatePartLive(
 	pieces = [...pieces, ...EffektTransitionPiece(context, config, partDefinition)]
 
 	EvaluateCues(context, config, pieces, adLibPieces, partDefinition.cues, partDefinition)
-	AddScript(partDefinition, pieces, partTime, false)
+	AddScript(partDefinition, pieces, partTime)
 
 	if (partDefinition.cues.filter(cue => cue.type === CueType.MOS || cue.type === CueType.Telefon).length) {
 		part.prerollDuration = config.studio.PilotPrerollDuration
