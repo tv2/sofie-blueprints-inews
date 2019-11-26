dc6d93bec45e41f4e360bd3ea2dc849dcfe25ceb
Jesper Stærkær <jesper@starkar.no>
2019-11-26 14:24:25 +0100

    feat: adds b-studio cams to IP1/2 adlibs

--- src/tv2_afvd_showstyle/migrations/sourcelayer-defaults.ts
+++ src/tv2_afvd_showstyle/migrations/sourcelayer-defaults.ts
@@ -232,7 +232,7 @@ const PGM: ISourceLayer[] = [
 		isRemoteInput: false,
 		isGuestInput: false,
 		activateKeyboardHotkeys:
-			'shift+f1,shift+f2,shift+f3,shift+f4,shift+f5,shift+1,shift+2,shift+3,shift+4,shift+5,shift+6,shift+7,shift+8,shift+9,shift+0',
+			'shift+f1,shift+f2,shift+f3,shift+f4,shift+f5,shift+alt+f1,shift+falt+2,shift+alt+f3,shift+alt+f4,shift+alt+f5,shift+1,shift+2,shift+3,shift+4,shift+5,shift+6,shift+7,shift+8,shift+9,shift+0',
 		clearKeyboardHotkey: '',
 		assignHotkeysToGlobalAdlibs: true,
 		isSticky: false,
@@ -254,7 +254,7 @@ const PGM: ISourceLayer[] = [
 		isRemoteInput: false,
 		isGuestInput: false,
 		activateKeyboardHotkeys:
-			'ctrl+f1,ctrl+f2,ctrl+f3,ctrl+f4,ctrl+f5,ctrl+1,ctrl+2,ctrl+3,ctrl+4,ctrl+5,ctrl+6,ctrl+7,ctrl+8,ctrl+9,ctrl+0',
+			'ctrl+f1,ctrl+f2,ctrl+f3,ctrl+f4,ctrl+f5,ctrl+alt+f1,ctrl+alt+f2,ctrl+alt+f3,ctrl+alt+f4,ctrl+alt+f5,ctrl+1,ctrl+2,ctrl+3,ctrl+4,ctrl+5,ctrl+6,ctrl+7,ctrl+8,ctrl+9,ctrl+0',
 		clearKeyboardHotkey: '',
 		assignHotkeysToGlobalAdlibs: true,
 		isSticky: false,
