6a01c2891af0c172b108f8be130ad45efc61af8f
Tom Lee <thomasslee97@gmail.com>
2019-11-27 12:07:18 +0000

    debug: More A/B debugs

--- src/tv2_afvd_studio/helpers/abPlayback.ts
+++ src/tv2_afvd_studio/helpers/abPlayback.ts
@@ -303,9 +303,14 @@ function updateObjectsToMediaPlayer(
 				// TODO: Change when adding more servers
 				context.warning(playerId.toString())
 				context.warning(typeof playerId)
+
+				obj.layer = playerId === 0 ? SisyfosLLAyer.SisyfosSourceServerA : SisyfosLLAyer.SisyfosSourceServerB
 			} else if (obj.lookaheadForLayer === SisyfosLLAyer.SisyfosSourceClipPending) {
 				// This works on the assumption that layer will contain lookaheadForLayer, but not the exact syntax.
 				// Hopefully this will be durable to any potential future core changes
+				context.warning(playerId.toString())
+				context.warning(typeof playerId)
+
 				const targetPlayer = playerId === 0 ? SisyfosLLAyer.SisyfosSourceServerA : SisyfosLLAyer.SisyfosSourceServerB
 
 				// TODO: Change when adding more servers
