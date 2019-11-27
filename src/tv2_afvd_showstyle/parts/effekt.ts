ed083c96491474faed94cfb72186a8e2b612874f
Tom Lee <thomasslee97@gmail.com>
2019-11-27 09:33:38 +0000

    test: Overlap + caspar preroll

--- src/tv2_afvd_showstyle/parts/effekt.ts
+++ src/tv2_afvd_showstyle/parts/effekt.ts
@@ -30,7 +30,7 @@ export function GetBreakerEffekt(
 					transitionKeepaliveDuration:
 						TimeFromFrames(Number(realBreaker.StartAlpha)) + config.studio.CasparPrerollDuration + 1,
 					transitionPrerollDuration: config.studio.CasparPrerollDuration,
-					autoNextOverlap: TimeFromFrames(Number(realBreaker.EndAlpha)),
+					autoNextOverlap: TimeFromFrames(Number(realBreaker.EndAlpha)) + config.studio.CasparPrerollDuration,
 					autoNext: true
 				}
 			}
