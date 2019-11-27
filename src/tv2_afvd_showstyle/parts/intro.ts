066fcbb827f549470660e80f201f15fe5dc71c56
Tom Lee <thomasslee97@gmail.com>
2019-11-27 07:46:11 +0000

    temp: Disable overlap

--- src/tv2_afvd_showstyle/parts/intro.ts
+++ src/tv2_afvd_showstyle/parts/intro.ts
@@ -11,7 +11,7 @@ import { EvaluateCues } from '../helpers/pieces/evaluateCues'
 import { AddScript } from '../helpers/pieces/script'
 import { PartDefinition, PartType } from '../inewsConversion/converters/ParseBody'
 import { CueDefinitionJingle, CueType } from '../inewsConversion/converters/ParseCue'
-import { GetBreakerEffekt } from './effekt'
+// import { GetBreakerEffekt } from './effekt'
 import { CreatePartInvalid } from './invalid'
 import { TimeFromFrames } from './time/frameTime'
 import { PartTime } from './time/partTime'
@@ -70,7 +70,7 @@ export function CreatePartIntro(
 
 	EvaluateCues(context, config, pieces, adLibPieces, partDefinition.cues, partDefinition)
 	AddScript(partDefinition, pieces, partTime)
-	part = { ...part, ...GetBreakerEffekt(context, config, partDefinition) }
+	// part = { ...part, ...GetBreakerEffekt(context, config, partDefinition) }
 
 	if (pieces.length === 0) {
 		part.invalid = true
