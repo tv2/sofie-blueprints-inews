09fdb3020af8378339baa7c22332b75866231fe1
Tom Lee <thomasslee97@gmail.com>
2019-11-28 18:34:14 +0000

    fix: Add targetCue to durations

--- src/tv2_afvd_showstyle/parts/kam.ts
+++ src/tv2_afvd_showstyle/parts/kam.ts
@@ -133,7 +133,11 @@ export function CreatePartKam(
 	EvaluateCues(context, config, pieces, adLibPieces, partDefinition.cues, partDefinition)
 	AddScript(partDefinition, pieces, partTime)
 
-	if (partDefinition.cues.filter(cue => cue.type === CueType.MOS || cue.type === CueType.Telefon).length) {
+	if (
+		partDefinition.cues.filter(
+			cue => cue.type === CueType.MOS || cue.type === CueType.Telefon || cue.type === CueType.TargetEngine
+		).length
+	) {
 		part.prerollDuration = config.studio.PilotPrerollDuration
 		part.transitionKeepaliveDuration = config.studio.PilotKeepaliveDuration
 			? Number(config.studio.PilotKeepaliveDuration)
