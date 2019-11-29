f9bdaa16c9abcbca0f2c2c8981926dd4d22ab2b0
Jesper Stærkær <jesper@starkar.no>
2019-11-29 09:22:27 +0100

    feat(sisyfos): falsy defaults in hide/show mic config

--- src/tv2_afvd_studio/sisyfosChannels.ts
+++ src/tv2_afvd_studio/sisyfosChannels.ts
@@ -3,159 +3,136 @@ import { SisyfosLLAyer } from './layers'
 export interface SisyfosChannel {
 	isPgm: 0 | 1 | 2
 	label: string
-	visibleInStudioA?: boolean
-	visibleInStudioB?: boolean
+	hideInStudioA?: boolean
+	hideInStudioB?: boolean
 }
 
 export const sisyfosChannels: { [key in SisyfosLLAyer]?: SisyfosChannel } = {
 	[SisyfosLLAyer.SisyfosSourceHost_1_ST_A]: {
 		isPgm: 0,
-		label: 'VÆRT 1',
-		visibleInStudioA: true
+		label: 'VÆRT 1'
 	},
 	[SisyfosLLAyer.SisyfosSourceHost_2_ST_A]: {
 		isPgm: 0,
-		label: 'VÆRT 2',
-		visibleInStudioA: true
+		label: 'VÆRT 2'
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_1_ST_A]: {
 		isPgm: 0,
-		label: 'GÆST 1',
-		visibleInStudioA: true
+		label: 'GÆST 1'
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_2_ST_A]: {
 		isPgm: 0,
-		label: 'GÆST 2',
-		visibleInStudioA: true
+		label: 'GÆST 2'
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_3_ST_A]: {
 		isPgm: 0,
-		label: 'GÆST 3',
-		visibleInStudioA: true
+		label: 'GÆST 3'
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_4_ST_A]: {
 		isPgm: 0,
-		label: 'GÆST 4',
-		visibleInStudioA: true
+		label: 'GÆST 4'
 	},
 	[SisyfosLLAyer.SisyfosSourceHost_1_ST_B]: {
 		isPgm: 0,
 		label: 'B-VÆRT 1',
-		visibleInStudioB: true
+		hideInStudioA: true
 	},
 	[SisyfosLLAyer.SisyfosSourceHost_2_ST_B]: {
 		isPgm: 0,
 		label: 'B-VÆRT 2',
-		visibleInStudioB: true
+		hideInStudioA: true
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_1_ST_B]: {
 		isPgm: 0,
 		label: 'B-GÆST 1',
-		visibleInStudioB: true
+		hideInStudioA: true
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_2_ST_B]: {
 		isPgm: 0,
 		label: 'B-GÆST 2',
-		visibleInStudioB: true
+		hideInStudioA: true
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_3_ST_B]: {
 		isPgm: 0,
 		label: 'B-GÆST 3',
-		visibleInStudioB: true
+		hideInStudioA: true
 	},
 	[SisyfosLLAyer.SisyfosSourceGuest_4_ST_B]: {
 		isPgm: 0,
 		label: 'B-GÆST 4',
-		visibleInStudioB: true
+		hideInStudioA: true
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_1]: {
 		isPgm: 0,
-		label: 'LIVE 1',
-		visibleInStudioA: true
+		label: 'LIVE 1'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_2]: {
 		isPgm: 0,
-		label: 'LIVE 2',
-		visibleInStudioA: true
+		label: 'LIVE 2'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_3]: {
 		isPgm: 0,
-		label: 'LIVE 3',
-		visibleInStudioA: true
+		label: 'LIVE 3'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_4]: {
 		isPgm: 0,
-		label: 'LIVE 4',
-		visibleInStudioA: true
+		label: 'LIVE 4'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_5]: {
 		isPgm: 0,
-		label: 'LIVE 5',
-		visibleInStudioA: true
+		label: 'LIVE 5'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_6]: {
 		isPgm: 0,
-		label: 'LIVE 6',
-		visibleInStudioA: true
+		label: 'LIVE 6'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_7]: {
 		isPgm: 0,
-		label: 'LIVE 7',
-		visibleInStudioA: true
+		label: 'LIVE 7'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_8]: {
 		isPgm: 0,
-		label: 'LIVE 8',
-		visibleInStudioA: true
+		label: 'LIVE 8'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_9]: {
 		isPgm: 0,
-		label: 'LIVE 9',
-		visibleInStudioA: true
+		label: 'LIVE 9'
 	},
 	[SisyfosLLAyer.SisyfosSourceLive_10]: {
 		isPgm: 0,
-		label: 'LIVE 10',
-		visibleInStudioA: true
+		label: 'LIVE 10'
 	},
 	[SisyfosLLAyer.SisyfosSourceServerA]: {
 		isPgm: 0,
-		label: 'Server A',
-		visibleInStudioA: true
+		label: 'Server A'
 	},
 	[SisyfosLLAyer.SisyfosSourceServerB]: {
 		isPgm: 0,
-		label: 'Server B',
-		visibleInStudioA: true
+		label: 'Server B'
 	},
 	// [SisyfosLLAyer.SisyfosSourceServerC]: {
 	// 	isPgm: 0,
 	// 	label: 'Server C',
-	// 	visibleInStudioA: false // for future applications
+	// 	hideInStudioA: false // for future applications
 	// },
 	[SisyfosLLAyer.SisyfosSourceEVS_1]: {
 		isPgm: 0,
-		label: 'EVS 1',
-		visibleInStudioA: true
+		label: 'EVS 1'
 	},
 	[SisyfosLLAyer.SisyfosSourceEVS_2]: {
 		isPgm: 0,
-		label: 'EVS 2',
-		visibleInStudioA: true
+		label: 'EVS 2'
 	},
 	[SisyfosLLAyer.SisyfosSourceJingle]: {
 		isPgm: 0,
-		label: 'JINGLE',
-		visibleInStudioA: true
+		label: 'JINGLE'
 	},
 	[SisyfosLLAyer.SisyfosSourceAudiobed]: {
 		isPgm: 0,
-		label: 'BED',
-		visibleInStudioA: true
+		label: 'BED'
 	},
 	[SisyfosLLAyer.SisyfosSourceTLF]: {
 		isPgm: 0,
-		label: 'TLF',
-		visibleInStudioA: true
+		label: 'TLF'
 	}
 }
