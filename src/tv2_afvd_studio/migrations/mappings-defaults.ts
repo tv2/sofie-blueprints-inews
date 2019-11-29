f8ec6f9a0894832b2aa84725a8d8686541a8d96b
Jesper Stærkær <jesper@starkar.no>
2019-11-29 09:55:06 +0100

    feat: allows DVE lookahead, we thinks

--- src/tv2_afvd_studio/migrations/mappings-defaults.ts
+++ src/tv2_afvd_studio/migrations/mappings-defaults.ts
@@ -193,21 +193,24 @@ export default literal<BlueprintMappings>({
 	atem_supersource_z_box1: literal<MappingAtem & BlueprintMapping>({
 		device: DeviceType.ATEM,
 		deviceId: 'atem0',
-		lookahead: LookaheadMode.NONE, // TODO - set to WHEN_CLEAR with a maxDepth of 1
+		lookahead: LookaheadMode.WHEN_CLEAR,
+		lookaheadDepth: 1,
 		mappingType: MappingAtemType.SuperSourceBox,
 		index: 0 // 0 = SS
 	}),
 	atem_supersource_z_box2: literal<MappingAtem & BlueprintMapping>({
 		device: DeviceType.ATEM,
 		deviceId: 'atem0',
-		lookahead: LookaheadMode.NONE, // TODO - set to WHEN_CLEAR with a maxDepth of 1
+		lookahead: LookaheadMode.WHEN_CLEAR,
+		lookaheadDepth: 1,
 		mappingType: MappingAtemType.SuperSourceBox,
 		index: 0 // 0 = SS
 	}),
 	atem_supersource_z_box3: literal<MappingAtem & BlueprintMapping>({
 		device: DeviceType.ATEM,
 		deviceId: 'atem0',
-		lookahead: LookaheadMode.NONE, // TODO - set to WHEN_CLEAR with a maxDepth of 1
+		lookahead: LookaheadMode.WHEN_CLEAR,
+		lookaheadDepth: 1,
 		mappingType: MappingAtemType.SuperSourceBox,
 		index: 0 // 0 = SS
 	}),
