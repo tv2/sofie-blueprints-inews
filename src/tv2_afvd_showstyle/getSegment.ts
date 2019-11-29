88f184a063418b5a33462f2e2d204ff2e1a9d20d
Tom Lee <thomasslee97@gmail.com>
2019-11-29 11:41:47 +0000

    fix: Lint

--- src/tv2_afvd_showstyle/getSegment.ts
+++ src/tv2_afvd_showstyle/getSegment.ts
@@ -1,7 +1,13 @@
-import { DeviceType, TimelineContentTypeAtem, TimelineObjAtemME } from 'timeline-state-resolver-types'
+import {
+	DeviceType,
+	TimelineContentTypeAtem,
+	TimelineObjAtemAny,
+	TimelineObjAtemME
+} from 'timeline-state-resolver-types'
 import {
 	BlueprintResultPart,
 	BlueprintResultSegment,
+	CameraContent,
 	IBlueprintPiece,
 	IBlueprintRundownDB,
 	IBlueprintSegment,
@@ -15,7 +21,13 @@ import * as _ from 'underscore'
 import { assertUnreachable, literal } from '../common/util'
 import { AtemLLayer } from '../tv2_afvd_studio/layers'
 import { BlueprintConfig, parseConfig } from './helpers/config'
-import { ParseBody, PartDefinition, PartDefinitionSlutord, PartType } from './inewsConversion/converters/ParseBody'
+import {
+	ParseBody,
+	PartDefinition,
+	PartDefinitionKam,
+	PartDefinitionSlutord,
+	PartType
+} from './inewsConversion/converters/ParseBody'
 import {
 	CueDefinitionGrafik,
 	CueDefinitionMOS,
