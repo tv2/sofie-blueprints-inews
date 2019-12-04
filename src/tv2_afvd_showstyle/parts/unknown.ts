ba4c065f8eaee36427b3f6c25834dc1268fbe8f6
Thomas Lee <thomasslee97@gmail.com>
2019-12-04 18:54:12 +0000

    fix: Ignore script

--- src/tv2_afvd_showstyle/parts/unknown.ts
+++ src/tv2_afvd_showstyle/parts/unknown.ts
@@ -34,7 +34,9 @@ export function CreatePartUnknown(
 	const pieces: IBlueprintPiece[] = []
 
 	EvaluateCues(context, config, pieces, adLibPieces, partDefinition.cues, partDefinition, asAdlibs)
-	AddScript(partDefinition, pieces, partTime)
+	if (!asAdlibs) {
+		AddScript(partDefinition, pieces, partTime)
+	}
 	part = { ...part, ...GetBreakerEffekt(context, config, partDefinition) }
 
 	if (
