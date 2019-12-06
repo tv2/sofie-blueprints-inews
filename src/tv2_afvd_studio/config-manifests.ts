3eb0d9dae1fe357e4879044acc7e2cf800667ed7
Thomas Lee <thomasslee97@gmail.com>
2019-12-06 11:55:54 +0000

    chore: Config default value

--- src/tv2_afvd_studio/config-manifests.ts
+++ src/tv2_afvd_studio/config-manifests.ts
@@ -164,7 +164,7 @@ export const studioConfigManifest: ConfigManifestEntry[] = [
 		description: 'ATEM source for mos full-frame grafik background source',
 		type: ConfigManifestEntryType.NUMBER,
 		required: false,
-		defaultVal: 3010
+		defaultVal: 30
 	},
 	{
 		id: 'AtemSource.Default',
