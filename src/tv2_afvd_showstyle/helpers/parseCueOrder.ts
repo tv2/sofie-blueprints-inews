76e710f63229f9b1cec1e27351b3d337358e5f72
Thomas Lee <thomasslee97@gmail.com>
2020-01-06 14:13:31 +0000

    fix: part externalIds

--- src/tv2_afvd_showstyle/helpers/parseCueOrder.ts
+++ src/tv2_afvd_showstyle/helpers/parseCueOrder.ts
@@ -1,6 +1,10 @@
 import { PartDefinition, PartType } from '../inewsConversion/converters/ParseBody'
 import { GetNextPartCue } from './nextPartCue'
 
+import * as md5 from 'md5'
+import { assertUnreachable } from '../../common/util'
+import { CueType, DVESources } from '../inewsConversion/converters/ParseCue'
+
 export function ParseCueOrder(partDefinitions: PartDefinition[], segmentId: string): PartDefinition[] {
 	const retDefintions: PartDefinition[] = []
 	let partIdCounter = 0
@@ -11,7 +15,7 @@ export function ParseCueOrder(partDefinitions: PartDefinition[], segmentId: stri
 		if (partDefinition.type !== PartType.Unknown) {
 			// No extra parts
 			if (first === -1) {
-				retDefintions.push({ ...partDefinition, externalId: `${segmentId}-${partIdCounter}` })
+				retDefintions.push({ ...partDefinition, externalId: getExternalId(segmentId, partDefinition, partIdCounter) })
 				partIdCounter++
 				return
 			} else {
@@ -21,10 +25,17 @@ export function ParseCueOrder(partDefinitions: PartDefinition[], segmentId: stri
 					cues: first > 0 ? partDefinition.cues.splice(0, first) : [],
 					externalId: `${segmentId}-${partIdCounter}`
 				})
+				const part = {
+					...partDefinition,
+					...(partDefinition.type === PartType.Slutord ? { script: '' } : {}),
+					cues: first > 0 ? partDefinition.cues.splice(0, first) : []
+				}
+				part.externalId = getExternalId(segmentId, part, partIdCounter)
+				retDefintions.push(part)
 				partIdCounter++
 			}
 		} else if (GetNextPartCue(partDefinition, -1) === -1) {
-			retDefintions.push({ ...partDefinition, externalId: `${segmentId}-${partIdCounter}` })
+			retDefintions.push({ ...partDefinition, externalId: getExternalId(segmentId, partDefinition, partIdCounter) })
 			partIdCounter++
 			return
 		}
@@ -39,10 +50,10 @@ export function ParseCueOrder(partDefinitions: PartDefinition[], segmentId: stri
 
 		while (partDefinition.cues.length) {
 			if (GetNextPartCue(partDefinition, 0) !== -1) {
-				retDefintions.push({
+				const part: PartDefinition = {
 					type: PartType.Unknown,
 					variant: {},
-					externalId: `${segmentId}-${partIdCounter}`,
+					externalId: '',
 					rawType: '',
 					cues: partDefinition.cues.splice(0, GetNextPartCue(partDefinition, 0)),
 					script:
@@ -54,12 +65,14 @@ export function ParseCueOrder(partDefinitions: PartDefinition[], segmentId: stri
 					fields: partDefinition.fields,
 					modified: partDefinition.modified,
 					storyName: partDefinition.storyName
-				})
+				}
+				part.externalId = getExternalId(segmentId, part, partIdCounter)
+				retDefintions.push(part)
 			} else {
-				retDefintions.push({
+				const part: PartDefinition = {
 					type: PartType.Unknown,
 					variant: {},
-					externalId: `${segmentId}-${partIdCounter}`,
+					externalId: ``,
 					rawType: '',
 					cues: partDefinition.cues,
 					script:
@@ -71,7 +84,9 @@ export function ParseCueOrder(partDefinitions: PartDefinition[], segmentId: stri
 					fields: partDefinition.fields,
 					modified: partDefinition.modified,
 					storyName: partDefinition.storyName
-				})
+				}
+				part.externalId = getExternalId(segmentId, part, partIdCounter)
+				retDefintions.push(part)
 				partDefinition.cues = []
 			}
 			isFirstNewPrimary = false
@@ -80,4 +95,90 @@ export function ParseCueOrder(partDefinitions: PartDefinition[], segmentId: stri
 	})
 
 	return retDefintions
+}
+
+function getExternalId(segmentId: string, partDefinition: PartDefinition, partNum: number): string {
+	let tail = `${partDefinition.type.toString()}-${partDefinition.rawType}`
+
+	switch (partDefinition.type) {
+		case PartType.EVS:
+			// Common pattern to see EV1 and EVS1VO in the same story
+			tail += `${partDefinition.variant.evs}-${partDefinition.variant.isVO}-${partNum}`
+			break
+		case PartType.Grafik:
+			// Grafik parts can reasonably be identified by their cues
+			tail += `${JSON.stringify(partDefinition.cues)}`
+			break
+		case PartType.INTRO:
+			// Intro must have a jingle cue
+			tail += `${JSON.stringify(partDefinition.cues.filter(cue => cue.type === CueType.Jingle))}`
+			break
+		case PartType.Kam:
+			// Reasonable that there will be more than one camera of the same variant
+			tail += `${JSON.stringify(partDefinition.variant.name)}-${partNum}`
+			break
+		case PartType.Server:
+			// Only one video Id per story
+			tail += `${partDefinition.fields.videoId}`
+			break
+		case PartType.Slutord:
+			// Slutord parts are filtered out before reaching core, so don't matter as much
+			tail += `${partDefinition.script}`
+			break
+		case PartType.Teknik:
+			// Possibly an unused part type, not seen in production - only one example found in original test data
+			tail += `TEKNIK`
+			break
+		case PartType.VO:
+			// Only one video Id per story.
+			tail += `${partDefinition.fields.videoId}`
+			break
+		case PartType.Unknown:
+			// Special cases based on cues (primary cues)
+			const firstCue = partDefinition.cues[0]
+
+			if (firstCue) {
+				switch (firstCue.type) {
+					case CueType.AdLib:
+						tail += `${firstCue.variant}`
+						break
+					case CueType.DVE:
+						function countSources(sources: DVESources) {
+							let count = 0
+							for (const [key, value] of Object.entries(sources)) {
+								if (key !== undefined && value !== undefined) {
+									count += 1
+								}
+							}
+
+							return count
+						}
+						tail += `${firstCue.template}-${countSources(firstCue.sources)}`
+						break
+					case CueType.Ekstern:
+						// Reasonable that the same live source wil appear more than once in a story
+						tail += `${firstCue.source}-${partNum}`
+						break
+					case CueType.Jingle:
+						tail += `${firstCue.clip}`
+						break
+					case CueType.TargetEngine:
+						// Pair the engine will the graphic, common to see 'FULL' targeted multiple times in one story
+						tail += `${firstCue.engine}-${JSON.stringify(firstCue.grafik)}`
+						break
+					case CueType.Telefon:
+						tail += `${firstCue.source}-${partNum}`
+						break
+				}
+			} else {
+				// This should never happen. Log it in case it ever occurs.
+				console.log(`Adding part with potentially bad Id: ${JSON.stringify(partDefinition)}`)
+				tail += `UNKNOWN`
+			}
+			break
+		default:
+			assertUnreachable(partDefinition)
+	}
+
+	return md5(`${segmentId}-${tail}`)
 }
