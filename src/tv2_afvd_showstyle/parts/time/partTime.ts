71cd3a5af0d2e97bc3ef30be83dfb8a4d61a1b44
Thomas Lee <thomasslee97@gmail.com>
2020-01-14 13:00:20 +0000

    fix: Use the correct timing field

--- src/tv2_afvd_showstyle/parts/time/partTime.ts
+++ src/tv2_afvd_showstyle/parts/time/partTime.ts
@@ -1,7 +1,7 @@
 import { PartDefinition } from '../../../tv2_afvd_showstyle/inewsConversion/converters/ParseBody'
 
 export function PartTime(partDefinition: PartDefinition, totalWords: number, defaultTime: boolean = true): number {
-	const storyDuration = Number(partDefinition.fields.audioTime) * 1000 || 0
+	const storyDuration = Number(partDefinition.fields.totalTime) * 1000 || 0
 	const partTime = (partDefinition.script.replace(/[\r\n]/g, '').length / totalWords) * storyDuration
 	return partTime > 0 ? partTime : defaultTime ? 3000 : 0
 }
