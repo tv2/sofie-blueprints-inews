a40996242b5872365a23bfab681cb39f18aa962c
Tom Lee <thomasslee97@gmail.com>
2020-03-12 14:04:08 +0000

    fix: Don't add script to parts that have no other pieces

--- src/tv2_afvd_showstyle/helpers/pieces/script.ts
+++ src/tv2_afvd_showstyle/helpers/pieces/script.ts
@@ -7,6 +7,8 @@ const PREVIEW_CHARACTERS = 30
 
 // export function AddScript(part: PartDefinition, pieces: IBlueprintPiece[], duration: number, slutord: boolean) {
 export function AddScript(part: PartDefinition, pieces: IBlueprintPiece[], duration: number) {
+	if (!pieces.length) return
+
 	if (duration === 0) {
 		duration = 1000
 	}
