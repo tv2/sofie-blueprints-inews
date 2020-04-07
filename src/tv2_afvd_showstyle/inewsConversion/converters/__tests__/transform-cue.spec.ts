d97cd1b53fa509f1fb4e299b405525e74df6ac87
Tom Lee <thomasslee97@gmail.com>
2020-04-07 15:32:19 +0100

    feat: VCP for all elements

--- src/tv2_afvd_showstyle/inewsConversion/converters/__tests__/transform-cue.spec.ts
+++ src/tv2_afvd_showstyle/inewsConversion/converters/__tests__/transform-cue.spec.ts
@@ -2,7 +2,27 @@ import { literal } from '../../../../common/util'
 import { ShowStyleConfig } from '../../../../tv2_afvd_showstyle/helpers/config'
 import { TransformCuesIntoShowstyle } from '../../TransformCuesIntoShowstyle'
 import { PartDefinitionKam, PartType } from '../ParseBody'
-import { CueDefinitionGrafik, CueDefinitionMOS, CueDefinitionTargetEngine, CueType } from '../ParseCue'
+import {
+	CueDefinitionGrafik,
+	CueDefinitionMOS,
+	CueDefinitionTargetEngine,
+	CueDefinitionVIZ,
+	CueType
+} from '../ParseCue'
+
+const mosCue: CueDefinitionMOS = {
+	type: CueType.MOS,
+	name: 'LgfxWeb/-ETKAEM_07-05-2019_17:55:42/Mosart=L|00:00|00:10',
+	vcpid: 2520177,
+	continueCount: -1,
+	start: {
+		seconds: 0
+	},
+	end: {
+		seconds: 10
+	},
+	iNewsCommand: 'VCP'
+}
 
 describe('TransformCuesIntoShowstyle', () => {
 	const config: ShowStyleConfig = {
@@ -33,6 +53,30 @@ describe('TransformCuesIntoShowstyle', () => {
 				Argument1: '',
 				Argument2: '',
 				IsDesign: false
+			},
+			{
+				VizTemplate: 'VCP',
+				SourceLayer: 'studio0_graphics_pilotOverlay',
+				LayerMapping: 'viz_layer_pilot_overlay',
+				INewsCode: '#kg',
+				INewsName: 'bund',
+				VizDestination: 'FULL',
+				OutType: '',
+				Argument1: '',
+				Argument2: '',
+				IsDesign: false
+			},
+			{
+				VizTemplate: 'VCP',
+				SourceLayer: 'studio0_graphics_pilotOverlay',
+				LayerMapping: 'viz_layer_pilot_overlay',
+				INewsCode: 'VIZ',
+				INewsName: 'bund',
+				VizDestination: 'OVL',
+				OutType: '',
+				Argument1: '',
+				Argument2: '',
+				IsDesign: false
 			}
 		],
 		WipesConfig: [],
@@ -50,19 +94,6 @@ describe('TransformCuesIntoShowstyle', () => {
 			rawType: `SS=SC-STILLS`,
 			content: {},
 			iNewsCommand: 'SS'
-		}
-		const mosCue: CueDefinitionMOS = {
-			type: CueType.MOS,
-			name: 'LgfxWeb/-ETKAEM_07-05-2019_17:55:42/Mosart=L|00:00|00:10',
-			vcpid: 2520177,
-			continueCount: -1,
-			start: {
-				seconds: 0
-			},
-			end: {
-				seconds: 10
-			},
-			iNewsCommand: 'VCP'
 		}
 		const partDefinition: PartDefinitionKam = {
 			type: PartType.Kam,
@@ -115,19 +146,6 @@ describe('TransformCuesIntoShowstyle', () => {
 			rawType: `SS=SC_LOOP`,
 			content: {},
 			iNewsCommand: 'SS'
-		}
-		const mosCue: CueDefinitionMOS = {
-			type: CueType.MOS,
-			name: 'LgfxWeb/-ETKAEM_07-05-2019_17:55:42/Mosart=L|00:00|00:10',
-			vcpid: 2520177,
-			continueCount: -1,
-			start: {
-				seconds: 0
-			},
-			end: {
-				seconds: 10
-			},
-			iNewsCommand: 'VCP'
 		}
 		const partDefinition: PartDefinitionKam = {
 			type: PartType.Kam,
@@ -188,19 +206,7 @@ describe('TransformCuesIntoShowstyle', () => {
 			content: {},
 			iNewsCommand: 'SS'
 		}
-		const mosCue: CueDefinitionMOS = {
-			type: CueType.MOS,
-			name: 'LgfxWeb/-ETKAEM_07-05-2019_17:55:42/Mosart=L|00:00|00:10',
-			vcpid: 2520177,
-			continueCount: -1,
-			start: {
-				seconds: 0
-			},
-			end: {
-				seconds: 10
-			},
-			iNewsCommand: 'VCP'
-		}
+
 		const partDefinition: PartDefinitionKam = {
 			type: PartType.Kam,
 			variant: {
@@ -224,6 +230,108 @@ describe('TransformCuesIntoShowstyle', () => {
 			},
 			externalId: '',
 			cues: [targetWallCue, mosCue],
+			rawType: 'Kam 1',
+			script: '',
+			fields: {},
+			modified: 0,
+			storyName: ''
+		})
+	})
+
+	test('Handles VCP for #kg (other cues)', () => {
+		const kgCue: CueDefinitionGrafik = {
+			type: CueType.Grafik,
+			iNewsCommand: '#kg',
+			template: 'bund',
+			cue: '#kg',
+			textFields: []
+		}
+
+		const partDefinition: PartDefinitionKam = {
+			type: PartType.Kam,
+			variant: {
+				name: '1'
+			},
+			externalId: '',
+			cues: [kgCue, mosCue],
+			rawType: 'Kam 1',
+			script: '',
+			fields: {},
+			modified: 0,
+			storyName: ''
+		}
+
+		const res = TransformCuesIntoShowstyle(config, partDefinition)
+
+		expect(res).toEqual({
+			type: PartType.Kam,
+			variant: {
+				name: '1'
+			},
+			externalId: '',
+			cues: [
+				literal<CueDefinitionTargetEngine>({
+					type: CueType.TargetEngine,
+					rawType: '#kg bund',
+					data: {
+						engine: 'FULL',
+						grafik: mosCue
+					},
+					iNewsCommand: '#kg',
+					content: {}
+				})
+			],
+			rawType: 'Kam 1',
+			script: '',
+			fields: {},
+			modified: 0,
+			storyName: ''
+		})
+	})
+
+	test('Handles VCP for VIZ= (other cues)', () => {
+		const kgCue: CueDefinitionVIZ = {
+			type: CueType.VIZ,
+			iNewsCommand: 'VIZ',
+			design: 'bund',
+			rawType: 'VIZ=bund',
+			content: {}
+		}
+
+		const partDefinition: PartDefinitionKam = {
+			type: PartType.Kam,
+			variant: {
+				name: '1'
+			},
+			externalId: '',
+			cues: [kgCue, mosCue],
+			rawType: 'Kam 1',
+			script: '',
+			fields: {},
+			modified: 0,
+			storyName: ''
+		}
+
+		const res = TransformCuesIntoShowstyle(config, partDefinition)
+
+		expect(res).toEqual({
+			type: PartType.Kam,
+			variant: {
+				name: '1'
+			},
+			externalId: '',
+			cues: [
+				literal<CueDefinitionTargetEngine>({
+					type: CueType.TargetEngine,
+					rawType: 'VIZ=bund',
+					data: {
+						engine: 'OVL',
+						grafik: mosCue
+					},
+					iNewsCommand: 'VIZ',
+					content: {}
+				})
+			],
 			rawType: 'Kam 1',
 			script: '',
 			fields: {},
