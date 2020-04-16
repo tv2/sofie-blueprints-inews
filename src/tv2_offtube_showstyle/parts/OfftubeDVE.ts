239e47c3ed5537c52baa900ade7924f71e49b837
Tom Lee <thomasslee97@gmail.com>
2020-04-16 10:39:06 +0100

    fix: Preroll for DVE

--- src/tv2_offtube_showstyle/parts/OfftubeDVE.ts
+++ src/tv2_offtube_showstyle/parts/OfftubeDVE.ts
@@ -6,6 +6,7 @@ import {
 	PartContext
 } from 'tv-automation-sofie-blueprints-integration'
 import { literal, PartDefinitionDVE } from 'tv2-common'
+import { CueType } from 'tv2-constants'
 import { OffTubeShowstyleBlueprintConfig } from '../helpers/config'
 import { OfftubeEvaluateCues } from '../helpers/EvaluateCues'
 
@@ -23,6 +24,14 @@ export function OfftubeCreatePartDVE(
 	const pieces: IBlueprintPiece[] = []
 	const adLibPieces: IBlueprintAdLibPiece[] = []
 	OfftubeEvaluateCues(context, config, pieces, adLibPieces, partDefinition.cues, partDefinition, { adlib: true })
+
+	if (partDefinition.cues.filter(cue => cue.type === CueType.DVE).length) {
+		part.prerollDuration = config.studio.CasparPrerollDuration
+	}
+
+	if (pieces.length === 0) {
+		part.invalid = true
+	}
 
 	return {
 		part,
