87b73b84452e63441403b1f6e6afdd79be0acd69
Tom Lee <thomasslee97@gmail.com>
2020-05-19 12:40:31 +0100

    feat: VO Parts

--- src/tv2_offtube_showstyle/parts/OfftubeVO.ts
+++ src/tv2_offtube_showstyle/parts/OfftubeVO.ts
@@ -1,7 +1,27 @@
-import { BlueprintResultPart, PartContext } from 'tv-automation-sofie-blueprints-integration'
-import { AddScript, CreateAdlibServer, CreatePartServerBase, PartDefinition } from 'tv2-common'
-import { AdlibTags, CueType, Enablers } from 'tv2-constants'
-import { OfftubeAtemLLayer, OfftubeCasparLLayer, OfftubeSisyfosLLayer } from '../../tv2_offtube_studio/layers'
+import { DeviceType, TimelineObjAbstractAny } from 'timeline-state-resolver-types'
+import {
+	BlueprintResultPart,
+	IBlueprintAdLibPiece,
+	IBlueprintPiece,
+	PartContext,
+	PieceLifespan,
+	PieceMetaData
+} from 'tv-automation-sofie-blueprints-integration'
+import {
+	AddScript,
+	CreateAdlibServer,
+	CreatePartServerBase,
+	literal,
+	MakeContentServer,
+	PartDefinition
+} from 'tv2-common'
+import { AdlibTags, CueType, Enablers, MEDIA_PLAYER_AUTO } from 'tv2-constants'
+import {
+	OfftubeAbstractLLayer,
+	OfftubeAtemLLayer,
+	OfftubeCasparLLayer,
+	OfftubeSisyfosLLayer
+} from '../../tv2_offtube_studio/layers'
 import { OffTubeShowstyleBlueprintConfig } from '../helpers/config'
 import { OfftubeEvaluateCues } from '../helpers/EvaluateCues'
 import { MergePiecesAsTimeline } from '../helpers/MergePiecesAsTimeline'
@@ -31,6 +51,33 @@ export function OfftubeCreatePartVO(
 
 	// TODO: EFFEKT
 	// part = { ...part, ...CreateEffektForpart(context, config, partDefinition, pieces) }
+
+	pieces.push(
+		literal<IBlueprintPiece>({
+			_id: '',
+			externalId: partDefinition.externalId,
+			name: file,
+			enable: { start: 0 },
+			outputLayerId: 'pgm',
+			sourceLayerId: OffTubeSourceLayer.PgmServer,
+			infiniteMode: PieceLifespan.OutOnNextPart,
+			metaData: literal<PieceMetaData>({
+				mediaPlayerSessions: [segmentExternalId]
+			}),
+			content: MakeContentServer(file, segmentExternalId, partDefinition, config, {
+				Caspar: {
+					ClipPending: OfftubeCasparLLayer.CasparPlayerClipPending
+				},
+				Sisyfos: {
+					ClipPending: OfftubeSisyfosLLayer.SisyfosSourceClipPending
+				},
+				ATEM: {
+					MEPGM: OfftubeAtemLLayer.AtemMEProgram
+				}
+			}),
+			adlibPreroll: config.studio.CasparPrerollDuration
+		})
+	)
 
 	let adlibServer = CreateAdlibServer(
 		config,
@@ -75,6 +122,57 @@ export function OfftubeCreatePartVO(
 	])
 
 	adLibPieces.push(adlibServer)
+
+	// TODO: Clean up
+	const adlibServerFlowProducer: IBlueprintAdLibPiece = CreateAdlibServer(
+		config,
+		0,
+		partDefinition.externalId,
+		MEDIA_PLAYER_AUTO,
+		partDefinition,
+		file,
+		false,
+		{
+			PgmServer: OffTubeSourceLayer.SelectedAdLibServer,
+			PgmVoiceOver: OffTubeSourceLayer.SelectedAdLibVoiceOver,
+			Caspar: {
+				ClipPending: OfftubeCasparLLayer.CasparPlayerClipPending
+			},
+			ATEM: {
+				MEPGM: OfftubeAtemLLayer.AtemMEClean
+			},
+			Sisyfos: {
+				ClipPending: OfftubeSisyfosLLayer.SisyfosSourceClipPending
+			}
+		},
+		{
+			isOfftube: true,
+			tagAsAdlib: true,
+			enabler: Enablers.OFFTUBE_ENABLE_SERVER
+		}
+	)
+	adlibServerFlowProducer.toBeQueued = true
+	adlibServerFlowProducer.tags = ['flow_producer']
+	adlibServerFlowProducer.canCombineQueue = false
+	adlibServerFlowProducer.name = file
+	adlibServerFlowProducer.infiniteMode = PieceLifespan.OutOnNextPart
+	adlibServerFlowProducer.externalId = `${adlibServerFlowProducer.externalId}-flowProducer`
+	adlibServerFlowProducer.content!.timelineObjects.push(
+		literal<TimelineObjAbstractAny>({
+			id: '',
+			enable: {
+				while: '1'
+			},
+			priority: 1,
+			layer: OfftubeAbstractLLayer.OfftubeAbstractLLayerPgmEnabler,
+			content: {
+				deviceType: DeviceType.ABSTRACT
+			},
+			classes: [Enablers.OFFTUBE_ENABLE_SERVER]
+		})
+	)
+
+	adLibPieces.push(adlibServerFlowProducer)
 
 	AddScript(partDefinition, pieces, duration, OffTubeSourceLayer.PgmScript)
 
