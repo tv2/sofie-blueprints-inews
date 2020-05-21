fbf212a3cb3efbdd07dd4e178e19cf32ee370582
Tom Lee <thomasslee97@gmail.com>
2020-05-21 10:37:12 +0100

    chore: Remove duplicate class declaration

--- src/tv2_offtube_showstyle/getSegment.ts
+++ src/tv2_offtube_showstyle/getSegment.ts
@@ -10,9 +10,7 @@ import {
 	BlueprintResultSegment,
 	CameraContent,
 	IBlueprintPiece,
-	IBlueprintRundownDB,
 	IngestSegment,
-	PartContext,
 	PieceLifespan,
 	SegmentContext
 } from 'tv-automation-sofie-blueprints-integration'
@@ -48,66 +46,6 @@ export function getSegment(context: SegmentContext, ingestSegment: IngestSegment
 	return {
 		segment: result.segment,
 		parts: result.parts
-	}
-}
-
-export class PartContext2 implements PartContext {
-	public readonly rundownId: string
-	public readonly rundown: IBlueprintRundownDB
-	private baseContext: SegmentContext
-	private externalId: string
-
-	constructor(baseContext: SegmentContext, externalId: string) {
-		this.baseContext = baseContext
-		this.externalId = externalId
-
-		this.rundownId = baseContext.rundownId
-		this.rundown = baseContext.rundown
-	}
-
-	/** PartContext */
-	public getRuntimeArguments() {
-		return this.baseContext.getRuntimeArguments(this.externalId) || {}
-	}
-
-	/** IShowStyleConfigContext */
-	public getShowStyleConfig() {
-		return this.baseContext.getShowStyleConfig()
-	}
-	public getShowStyleConfigRef(configKey: string) {
-		return this.baseContext.getShowStyleConfigRef(configKey)
-	}
-
-	/** IStudioContext */
-	public getStudioMappings() {
-		return this.baseContext.getStudioMappings()
-	}
-
-	/** IStudioConfigContext */
-	public getStudioConfig() {
-		return this.baseContext.getStudioConfig()
-	}
-	public getStudioConfigRef(configKey: string) {
-		return this.baseContext.getStudioConfigRef(configKey)
-	}
-
-	/** NotesContext */
-	public error(message: string) {
-		return this.baseContext.error(message)
-	}
-	public warning(message: string) {
-		return this.baseContext.warning(message)
-	}
-	public getNotes() {
-		return this.baseContext.getNotes()
-	}
-
-	/** ICommonContext */
-	public getHashId(originString: string, originIsNotUnique?: boolean) {
-		return this.baseContext.getHashId(`${this.externalId}_${originString}`, originIsNotUnique)
-	}
-	public unhashId(hash: string) {
-		return this.baseContext.unhashId(hash)
 	}
 }
 
