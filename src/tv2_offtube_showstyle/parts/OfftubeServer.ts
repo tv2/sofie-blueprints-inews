07e07ff238ac2b4611067e2e6d513f59df5f897b
Tom Lee <thomasslee97@gmail.com>
2020-05-27 14:33:23 +0100

    fix: Add kommentator tag to server adlibs

--- src/tv2_offtube_showstyle/parts/OfftubeServer.ts
+++ src/tv2_offtube_showstyle/parts/OfftubeServer.ts
@@ -101,7 +101,7 @@ export function OfftubeCreatePartServer(
 	)
 	adlibServer.toBeQueued = true
 	adlibServer.canCombineQueue = true
-	adlibServer.tags = [AdlibTags.OFFTUBE_100pc_SERVER]
+	adlibServer.tags = [AdlibTags.OFFTUBE_100pc_SERVER, AdlibTags.ADLIB_KOMMENTATOR]
 	adlibServer.name = file
 	// TODO: This should happen in above function
 	adlibServer.expectedDuration = duration
