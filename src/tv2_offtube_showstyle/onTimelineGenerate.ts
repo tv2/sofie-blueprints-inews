8a83f97c8dae14ba97805d1088d027afeb1733fb
Julian Waller <git@julusian.co.uk>
2020-06-06 15:44:40 +0100

    fix: disable the SelectedAdLibServer piece when there is no PgmServer in the part to ignore the enabling class bleeding through

--- src/tv2_offtube_showstyle/onTimelineGenerate.ts
+++ src/tv2_offtube_showstyle/onTimelineGenerate.ts
@@ -11,9 +11,11 @@ import {
 	CasparPlayerClip,
 	OfftubeAtemLLayer,
 	OfftubeCasparLLayer,
-	OfftubeSisyfosLLayer
+	OfftubeSisyfosLLayer,
+	OfftubeAbstractLLayer
 } from '../tv2_offtube_studio/layers'
 import { parseConfig } from './helpers/config'
+import { OfftubeSourceLayer } from './layers'
 
 export function onTimelineGenerateOfftube(
 	context: PartEventContext,
@@ -22,6 +24,33 @@ export function onTimelineGenerateOfftube(
 	previousPartEndState: PartEndState | undefined,
 	resolvedPieces: IBlueprintPieceDB[]
 ): Promise<BlueprintResultTimeline> {
+	// If we are not in a server, then disable the server adlib piece
+	const currentPartId = context.part._id
+	const currentPieces: { [id: string]: IBlueprintPieceDB } = {}
+	for (const piece of resolvedPieces) {
+		if (piece.partId === currentPartId) {
+			currentPieces[piece._id] = piece
+		}
+	}
+	const currentServerOnAir = Object.values(currentPieces).find(p => p.sourceLayerId === OfftubeSourceLayer.PgmServer)
+	if (!currentServerOnAir) {
+		const currentAdlibServerPieceGroup = timeline.find(
+			obj =>
+				obj.isGroup && obj.layer === OfftubeSourceLayer.SelectedAdLibServer && obj.pieceId && currentPieces[obj.pieceId]
+		)
+		if (currentAdlibServerPieceGroup) {
+			const enableObj = timeline.find(
+				obj =>
+					(obj as any).inGroup === currentAdlibServerPieceGroup.id &&
+					obj.layer === OfftubeAbstractLLayer.OfftubeAbstractLLayerServerEnable
+			)
+			if (enableObj) {
+				// This is the master object that looks for the class coming from OfftubeSourceLayer.PgmServer to say it is on air. We know that doesnt exist here, so ignore it
+				enableObj.enable = { while: '0' }
+			}
+		}
+	}
+
 	return onTimelineGenerate(
 		context,
 		timeline,
