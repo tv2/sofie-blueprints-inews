6d2a271c9d30a54c1db976077a929a2b5a3a6d43
Tom Lee <thomasslee97@gmail.com>
2020-06-22 13:28:30 +0100

    test: Move clips path to fileName

--- src/tv2-common/content/server.ts
+++ src/tv2-common/content/server.ts
@@ -45,8 +45,8 @@ export function MakeContentServer<
 	const filePath = `${SanitizePath(config.studio.ClipBasePath)}/${file}`
 	return literal<VTContent>({
 		studioLabel: '',
-		fileName: file, // playing casparcg
-		path: `${config.studio.NetworkBasePath}\\${config.studio.ClipBasePath}\\${file}${config.studio.ClipFileExtension}`, // full path on the source network storage
+		fileName: filePath, // playing casparcg
+		path: `${config.studio.NetworkBasePath}\\${file}${config.studio.ClipFileExtension}`, // full path on the source network storage
 		mediaFlowIds: [config.studio.MediaFlowId],
 		firstWords: '',
 		lastWords: '',
