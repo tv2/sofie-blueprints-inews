669047e46a4d2b000a25e601b13868148bf68f09
ianshade <zegzulakrzysztof@gmail.com>
2020-09-01 16:58:03 +0200

    Revert "fix: ident piece lifespan"
    This reverts commit ef260583d85180078d73191b311f284e894681c7.

--- src/tv2_afvd_showstyle/helpers/pieces/grafikViz.ts
+++ src/tv2_afvd_showstyle/helpers/pieces/grafikViz.ts
@@ -210,7 +210,7 @@ export function GetInfiniteModeForGrafik(
 		: isTlf
 		? PieceLifespan.WithinPart
 		: isIdent
-		? LifeSpan(parsedCue.end?.infiniteMode, PieceLifespan.WithinPart)
+		? PieceLifespan.OutOnSegmentEnd
 		: parsedCue.end && parsedCue.end.infiniteMode
 		? LifeSpan(parsedCue.end.infiniteMode, PieceLifespan.WithinPart)
 		: FindInfiniteModeFromConfig(config, parsedCue)
