310e4143e81521b9c8e9ff0d1fb2db7d8a14c09a
Tom Lee <thomasslee97@gmail.com>
2021-04-13 12:07:12 +0100

    fix: Allow syncing to graphics layers

--- src/tv2-common/updatePolicies/syncIngestUpdateToPartInstance.ts
+++ src/tv2-common/updatePolicies/syncIngestUpdateToPartInstance.ts
@@ -3,6 +3,7 @@ import {
 	BlueprintSyncIngestPartInstance,
 	SyncIngestUpdateToPartInstanceContext
 } from '@sofie-automation/blueprints-integration'
+import { SharedSourceLayers } from 'tv2-constants'
 import * as _ from 'underscore'
 import { stopOrReplaceAlwaysEditablePieces, updateAdLibInstances } from './index'
 
@@ -20,7 +21,21 @@ export function syncIngestUpdateToPartInstanceBase(
 		preSteps()
 	}
 
-	stopOrReplaceAlwaysEditablePieces(context, existingPartInstance, newPart, freelyEditableLayers)
+	const editableLayers = [
+		...freelyEditableLayers,
+		SharedSourceLayers.PgmGraphicsHeadline,
+		SharedSourceLayers.PgmGraphicsIdent,
+		SharedSourceLayers.PgmGraphicsIdentPersistent,
+		SharedSourceLayers.PgmGraphicsLower,
+		SharedSourceLayers.PgmGraphicsOverlay,
+		SharedSourceLayers.PgmGraphicsTLF,
+		SharedSourceLayers.PgmGraphicsTema,
+		SharedSourceLayers.PgmGraphicsTop,
+		SharedSourceLayers.PgmPilot,
+		SharedSourceLayers.PgmPilotOverlay
+	]
+
+	stopOrReplaceAlwaysEditablePieces(context, existingPartInstance, newPart, editableLayers)
 	updateAdLibInstances(context, existingPartInstance, newPart)
 
 	if (postSteps) {
