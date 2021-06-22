1b1beff9c57ec11fdf85adcf90433f10d64f7786
Tom Lee <thomasslee97@gmail.com>
2021-06-22 14:10:08 +0100

    fix: Enable miniShelf
    This reverts commit 4c1d53877bf549a4d5289acb158b766f91c64e27.
    
    Revert "fix: disable minishelf"
    
    This reverts commit 70723271162a372b5507812d0988216d091f3f6e.

--- src/tv2-common/getSegment.ts
+++ src/tv2-common/getSegment.ts
@@ -114,7 +114,6 @@ export function getSegmentBase<
 	const segment = literal<IBlueprintSegment>({
 		name: ingestSegment.name || '',
 		metaData: {},
-		showShelf: true,
 		identifier:
 			iNewsStory && iNewsStory.fields.pageNumber && iNewsStory.fields.pageNumber.trim()
 				? iNewsStory.fields.pageNumber.trim()
