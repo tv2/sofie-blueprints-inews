e82b402acfa85f5ae0543a3b15fe26d26e21629f
ianshade <zegzulakrzysztof@gmail.com>
2021-10-20 13:09:35 +0200

    fix: make rundown duration calculation use `untimed` prop coming from the gateway

--- src/inews-mixins/rundownDuration.ts
+++ src/inews-mixins/rundownDuration.ts
@@ -5,12 +5,8 @@ export function getRundownDuration(segments: IngestSegment[]) {
 	let totalTime = 0
 	for (const segment of segments.sort((a, b) => a.rank - b.rank)) {
 		const payload = segment.payload as INewsPayload | undefined
-		if (payload?.iNewsStory?.meta?.float === 'float') {
+		if (payload?.iNewsStory?.meta?.float === 'float' || payload?.untimed) {
 			continue
-		}
-
-		if (segment.name.match(/^\s*continuity\s*$/i) && payload?.iNewsStory?.fields?.backTime?.match(/^@\d+$/)) {
-			break
 		}
 
 		const time = payload?.iNewsStory?.fields.totalTime ?? 0
