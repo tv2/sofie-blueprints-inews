f0962fb227ea30b3b3312e0d23c5d7ced95a768d
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2022-01-31 12:41:34 +0100

    Simplify commands for convert.js - Possible to specify another studioId for upload script

--- rundowns/convert.js
+++ rundowns/convert.js
@@ -1,8 +1,8 @@
 // this converts a snapshot into importable rundown data
-// use with `node convert.js snapshot.json mock-rundown.json`
-if (process.argv.length < 4) {
+// use with `node convert.js snapshot.json`
+if (process.argv.length < 3) {
     console.log(process.argv)
-    console.log(`Usage: node convert.js snapshot.json mock-rundown.json`)
+    console.log(`Usage: node convert.js snapshot.json`)
     process.exit(0)
 }
 const fs = require('fs')
@@ -26,5 +26,5 @@ segments = segments.sort((a, b) => b.rank - a.rank)
 const rundown = rundownData[0].data
 rundown.segments = segments
 // console.log(JSON.stringify(rundown, undefined, 4))
-fs.writeFileSync(process.argv[3], JSON.stringify(rundown, undefined, 4))
+fs.writeFileSync('output.json', JSON.stringify(rundown, undefined, 4))
 console.log('done')
--- rundowns/upload.sh
+++ rundowns/upload.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-curl -ks --data-binary @$1 --header 'Content-Type:application/json' http://localhost:3000/ingest/studio0
+STUDIO_ID=studio0
+if [[ $1 != "" ]]
+then
+  STUDIO_ID=$1
+fi
+
+curl -ks --data-binary @output.json --header 'Content-Type:application/json' http://localhost:3000/ingest/${STUDIO_ID}
 
 echo "\n"
