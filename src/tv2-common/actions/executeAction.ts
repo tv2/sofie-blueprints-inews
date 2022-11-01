50087b14ef87f02b2e50b0a19e4f8ca3aa40bdd8
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2022-11-01 13:48:40 +0100

    fix: SOF-1141 Wrap last recalled Ident for DVE in a new object before inserting it

--- src/tv2-common/actions/executeAction.ts
+++ src/tv2-common/actions/executeAction.ts
@@ -1916,13 +1916,14 @@ async function executeActionRecallLastDVE<
 
 	if (lastPlayedScheduledDVE && isLastPlayedAScheduledDVE) {
 		await scheduleLastPlayedDVE(context, settings, actionId, lastPlayedScheduledDVE)
-		await addLatestPieceOnLayerForDve(context, settings.SourceLayers.Ident, lastPlayedScheduledDVE.piece)
+		await addLatestPieceOnLayerForDve(context, settings.SourceLayers.Ident, actionId, lastPlayedScheduledDVE.piece)
 	}
 }
 
 async function addLatestPieceOnLayerForDve(
-	context: IActionExecutionContext,
+	context: ITV2ActionExecutionContext,
 	layer: string,
+	actionId: string,
 	dvePiece: IBlueprintPiece
 ): Promise<void> {
 	const lastIdent = await context.findLastPieceOnLayer(layer, {
@@ -1938,7 +1939,14 @@ async function addLatestPieceOnLayerForDve(
 		return
 	}
 
-	await context.insertPiece('next', lastIdent.piece)
+	const externalId = generateExternalId(context, actionId, [dvePiece.name])
+	const newIdentPiece: IBlueprintPiece<PieceMetaData> = {
+		...lastIdent.piece,
+		externalId,
+		lifespan: PieceLifespan.WithinPart
+	}
+
+	await context.insertPiece('next', newIdentPiece)
 }
 
 async function executeActionFadeDownPersistedAudioLevels<
